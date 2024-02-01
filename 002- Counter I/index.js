ebf9fdd84041529b6a0a446bfe4d91ebb11d1bb9
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-01 23:47:27 +0430

    Day 2 - Counter I

---
+++ 002- Counter I/index.js
@@ -0,0 +1,8 @@
+// Task: Given an integer n, return a counter function.
+// This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+let createCounter = (n) => {
+  return () => {
+    return n++;
+  };
+};
